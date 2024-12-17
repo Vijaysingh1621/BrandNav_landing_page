1d09e88dcf02c61894cc1c1c8ae60d4259daeea9
Vijay Singh <rockcodingrookie21@gmail.com>
2024-12-17 20:40:50 +0530

    hero_changed

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -18,26 +18,24 @@ export function Hero() {
       style={{
         backgroundImage: `url(${bgline.src})`, // i used this bg but it is giving white shadows and black bg so didnt use took from figma 
       }} >
-       <h1 className="text-[2.5rem] sm:text-[3.5rem] md:text-[4rem] lg:text-[56px] font-[540] leading-tight text-[#0D2052] max-w-4xl mx-auto mb-6 text-left sm:text-center">
-        Weapons to grow<br />
-        your Business
-        </h1>
+       <h2 className="text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.2rem] font-medium leading-tight text-[#0D2052] max-w-4xl mx-auto mb-6 text-left sm:text-center">
+      Weapons to grow <br className="hidden sm:inline" />
+      your Business
+      </h2>
         <div className="max-w-2xl mx-auto space-y-1 mb-8 mt-0 gap-0  text-left sm:text-center">
-        <p className="text-gray-600 sm:text-xl lg:text-[16px] ">
+        <p className="text-gray-600 sm:text-xl lg:text-[15px] ">
             Turbocharge your business growth.
         </p>
-        <p className="text-gray-600 sm:text-xl lg:text-[16px] ">
+        <p className="text-gray-600 sm:text-xl lg:text-[15px] ">
             Our products help ignite sales and entrepreneurship.
         </p>
         </div>
 
-       
-       <div> 
-       <button className="bg-gradient-to-r from-[#52A8FF] via-[#0084FF] to-[#0084FF] text-white lg:px-12 w-full lg:w-auto mx-0 py-[8px] rounded-[10px] text-lg font-medium hover:bg-blue-600 transition-colors mb-2">
-          Start for free
-        </button>
+        <button className="bg-gradient-to-r from-[#52A8FF] via-[#0084FF] to-[#0084FF] text-white w-full md:w-full lg:w-auto lg:px-12 py-[8px] rounded-[10px] text-lg font-medium hover:bg-blue-600 transition-colors mb-3">
+      Start for free
+    </button>
         
-        <div className="flex items-center justify-center gap-4 text-sm text-gray-600 hidden lg:flex md:flex">
+        <div className="flex items-center justify-center gap-4 text-[13px] text-gray-600 hidden mb-3 lg:flex md:flex">
           <div className="flex items-center gap-2">
             <Image src={CreditCard} alt="card"/>
                 
@@ -52,47 +50,42 @@ export function Hero() {
         
 
        </div>
-  
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4  w-full max-w-6xl mx-auto p-2  rounded-xl">
-            
 
-      {/* Verifier Card */}
-      <div className="bg-white rounded-xl shadow-lg items-center justify-center h-auto ">
+       
+       <div> 
+       
+  
+       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 h-full w-full max-w-6xl mx-auto  rounded-xl items-center justify-center w-full h-auto h-auto">
+      
         <Image
           src={verifierCard}
           alt="Verifier Card"
-          className="w-full h-auto object-contain lg:mt-[-80px]  mt-[0px]"
+          className="w-full h-auto object-contain lg:w-auto lg:mt-[-80px]"
         />
-      </div>
-
-      {/* Screener Card */}
-      <div className=" bg-white rounded-xl shadow-lg flex items-center justify-center">
+      
+      
         <Image
           src={screenerCard}
           alt="Screener Card"
-          className="w-full h-auto object-contain"
+          className="w-full h-auto object-contain lg:w-auto"
         />
-      </div>
-
-      {/* Black Shield Card */}
-      <div className=" bg-white rounded-xl shadow-lg  flex items-center justify-center">
+      
+      
         <Image
           src={blackShieldCard}
           alt="Black Shield Card"
-          className="w-full h-auto object-contain"
+          className="w-full h-auto object-contain lg:w-auto"
         />
-      </div>
-
-      {/* Enricher Card */}
-      <div className=" bg-white rounded-xl shadow-lg  flex items-center justify-center  ">
+      
+      
         <Image
           src={enricherCard}
           alt="Enricher Card"
-          className="w-full h-auto object-contain lg:mt-[-80px]  mt-[0px]"
+          className="w-full h-auto object-contain lg:w-auto lg:mt-[-80px]"
         />
-      </div>
-        </div>
-        </div>
+      
+    </div>
+    </div>
         <div className="p-8">
                 <h4 className="lg:text-[18px] text-[14px] md:text-[15px] font-poppins text-[#666666] mb-[9px] p-5">
                     Modern companies are using <span className="font-semibold">BrandNav</span>
