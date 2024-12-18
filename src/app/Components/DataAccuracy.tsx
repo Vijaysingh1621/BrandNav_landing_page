7804f4c3d16f119a9ceadf625be40c8b28d5f6d8
Vijay Singh <rockcodingrookie21@gmail.com>
2024-12-18 18:19:43 +0530

    fix_2

--- src/app/Components/DataAccuracy.tsx
+++ src/app/Components/DataAccuracy.tsx
@@ -10,32 +10,18 @@ export default function DataAccuracy() {
         <h1 className="text-2xl md:text-[28px] font-bold text-[#2A2665] mb-10 max-w-2xl mx-auto text-left sm:text-center pl-10">
           Fix data accuracy problems<br />with BrandNav
         </h1>
-        <div className="flex justify-center items-center gap-8 lg:gap-5 mx-auto max-w-6xl p-4 lg:p-8">
-  {/* Before Section */}
-  <div className="relative flex flex-col ">
-   
-    <Image
-      src={before}
-      height={280}
-      width={270}
-      alt="Before"
-      className="object-contain"
-    />
-  </div>
 
-  {/* After Section */}
-  <div className="relative flex flex-col">
-    
-    <Image
-      src={after}
-      height={320}
-      width={320}
-      alt="After"
-      className="object-contain mt-[-49px]"
-    />
-  </div>
-</div>
-
+         {/* Before/After Container */}
+         <div className="grid md:grid-cols-2 gap-4 lg:gap-0 max-w-6xl mx-auto h-full w-full lg:mx-[100px] lg:pl-[180px] ">
+            <div className="flex justify-center items-center px-[20px]  ">
+                <Image src={before} height={380} width={370} alt="" className="lg:mr-[-30px] "/>
+           
+            </div>
+            <div className="  ">
+                <Image src={after} height={450} width={450} alt="" className="ml-14 lg:m-[-10px] lg:mt-[2px] "/>
+            </div>
+          
+          </div>
 
        </div>
       </div>
