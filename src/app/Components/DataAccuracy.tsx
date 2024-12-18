e61b4a3d47f81b0b9503e8a999099375515bff50
Vijay Singh <rockcodingrookie21@gmail.com>
2024-12-18 18:11:59 +0530

    fix_

--- src/app/Components/DataAccuracy.tsx
+++ src/app/Components/DataAccuracy.tsx
@@ -10,18 +10,32 @@ export default function DataAccuracy() {
         <h1 className="text-2xl md:text-[28px] font-bold text-[#2A2665] mb-10 max-w-2xl mx-auto text-left sm:text-center pl-10">
           Fix data accuracy problems<br />with BrandNav
         </h1>
+        <div className="flex justify-center items-center gap-8 lg:gap-5 mx-auto max-w-6xl p-4 lg:p-8">
+  {/* Before Section */}
+  <div className="relative flex flex-col ">
+   
+    <Image
+      src={before}
+      height={280}
+      width={270}
+      alt="Before"
+      className="object-contain"
+    />
+  </div>
 
-         {/* Before/After Container */}
-         <div className="grid md:grid-cols-2 gap-4 lg:gap-0 max-w-6xl mx-auto h-full w-full lg:mx-[100px] lg:px-[100px]">
-            <div className="flex justify-center items-center px-0  ">
-                <Image src={before} height={380} width={370} alt="" className="lg:mr-[-30px] "/>
-           
-            </div>
-            <div className="  ">
-                <Image src={after} height={450} width={450} alt="" className="ml-14 lg:ml-[-10px] "/>
-            </div>
-          
-          </div>
+  {/* After Section */}
+  <div className="relative flex flex-col">
+    
+    <Image
+      src={after}
+      height={320}
+      width={320}
+      alt="After"
+      className="object-contain mt-[-49px]"
+    />
+  </div>
+</div>
+
 
        </div>
       </div>
